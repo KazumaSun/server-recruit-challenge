4fc33005f293a2aec0c99a5ea506bcbfa30fbe5d
KazumaSun <satoma.2001.0920@gmail.com>
2024-11-17 14:47:50 +0000

    [Feat]アルバムの情報に歌手の詳細情報を追加

--- infra/mysqldb/album.go
+++ infra/mysqldb/album.go
@@ -3,6 +3,8 @@ package mysqldb
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"log"
 
 	"github.com/pulse227/server-recruit-challenge-sample/model"
 	"github.com/pulse227/server-recruit-challenge-sample/repository"
@@ -20,18 +22,23 @@ type albumRepository struct {
 
 var _ repository.AlbumRepository = (*albumRepository)(nil)
 
-func (r *albumRepository) GetAll(ctx context.Context) ([]*model.Album, error) {
-	albums := []*model.Album{}
-	query := "SELECT id, title, singer_id FROM albums ORDER BY id ASC"
+func (r *albumRepository) GetAll(ctx context.Context) ([]*model.AlbumDetail, error) {
+	albums := []*model.AlbumDetail{}
+	var singerJSON string
+	query := "SELECT albums.id AS id, albums.title AS title, JSON_OBJECT( 'id', singers.id, 'name', singers.name) AS singer FROM albums INNER JOIN singers ON albums.singer_id = singers.id ORDER BY id ASC"
 	rows, err := r.db.QueryContext(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
-		album := &model.Album{}
-		if err := rows.Scan(&album.ID, &album.Title, &album.SingerID); err != nil {
+		album := &model.AlbumDetail{}
+		if err := rows.Scan(&album.ID, &album.Title, &singerJSON); err != nil {
 			return nil, err
+		}
+		err = json.Unmarshal([]byte(singerJSON), &album.Singer)
+		if err != nil {
+			log.Fatal(err)
 		}
 		if album.ID != 0 {
 			albums = append(albums, album)
@@ -44,17 +51,22 @@ func (r *albumRepository) GetAll(ctx context.Context) ([]*model.Album, error) {
 	return albums, nil
 }
 
-func (r *albumRepository) Get(ctx context.Context, id model.AlbumID) (*model.Album, error) {
-	album := &model.Album{}
-	query := "SELECT id, title, singer_id FROM albums WHERE id = ?"
+func (r *albumRepository) Get(ctx context.Context, id model.AlbumID) (*model.AlbumDetail, error) {
+	album := &model.AlbumDetail{}
+	var singerJSON string
+	query := "SELECT albums.id AS id, albums.title AS title, JSON_OBJECT( 'id', singers.id, 'name', singers.name) AS singer FROM albums INNER JOIN singers ON albums.singer_id = singers.id WHERE albums.id = ?"
 	rows, err := r.db.QueryContext(ctx, query, id)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
-		if err := rows.Scan(&album.ID, &album.Title, &album.SingerID); err != nil {
+		if err := rows.Scan(&album.ID, &album.Title, &singerJSON); err != nil {
 			return nil, err
+		}
+		err = json.Unmarshal([]byte(singerJSON), &album.Singer)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 	if err = rows.Err(); err != nil {
